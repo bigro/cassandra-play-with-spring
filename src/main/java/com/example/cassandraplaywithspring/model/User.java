3606aa3c1f253168793cd401ae347ae4d6721c55
t-yoguro <kitkat.jaja@gmail.com>
2019-11-14 16:07:42 +0900

    @Columnを使用

--- src/main/java/com/example/cassandraplaywithspring/model/User.java
+++ src/main/java/com/example/cassandraplaywithspring/model/User.java
@@ -1,5 +1,6 @@
 package com.example.cassandraplaywithspring.model;
 
+import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
@@ -11,8 +12,10 @@ public class User {
     @PrimaryKey("user_id")
     private UUID userId;
 
+    @Column("name") // @Columnはフィールド名と同じならなくてもいい
     private String name;
 
+    @Column("strength")
     private int strength;
 
     public User(UUID userId, String name, int strength) {
